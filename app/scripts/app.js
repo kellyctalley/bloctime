86e2842cc1fa3ab55e3c04074d970e7ede95eca8
Kelly Talley <kcutchen@gmail.com>
2015-06-10 02:15:11 +0000

    Timer and button working and resetting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,46 +9,27 @@ blocTime.controller("countdownTimer", ['$scope', '$interval', function($scope, $
 
   $scope.counter = 1500;
   var stop;
+  var isTimerRunning = false;
 
   $scope.startTimer = function() {
+    this.isTimerRunning = true;
     stop = $interval(function() {
       $scope.counter--;
+      if ($scope.counter == 0) {
+        $interval.cancel(stop);
+        $scope.counter = 1500;
+      }
     }, 1000);
-  };
 
+  };
 
   $scope.stopTimer = function() {
     $interval.cancel(stop);
+    $scope.counter = 1500;
+    this.isTimerRunning = false;
   }
 
 }]);
-
-/*blocTime.controller("counterCtrl",['$scope','$timeout', function($scope,$timeout){
-
- //Adding initial value for counter
-$scope.counter = 2500;
-var stopped;
-
-//timeout function
-//1000 milliseconds = 1 second
-//Every second counts
-//Cancels a task associated with the promise. As a result of this, the //promise will be resolved with a rejection.  
-$scope.countdown = function() {
-    stopped = $timeout(function() {
-       console.log($scope.counter);
-     $scope.counter--;   
-     $scope.countdown();   
-    }, 1000);
-  };
-   
-    
-$scope.stop = function(){
-   $timeout.cancel(stopped);
-    
-    } 
-
-
-}]); */
 
 
 //create button with ngClick directive to start or reset timer
